f5b87d708207677a04c1b9e6e635eb9eb35f321c
angelo <159036003+angelo5628@users.noreply.github.com>
2025-09-25 23:10:55 -0300

    Create comandas.py

---
+++ comandas.py
@@ -0,0 +1,11 @@
+ultimo_id = 0
+id_comanda = ultimo_id + 1
+
+mesa_id = int(input("ID da mesa: "))
+funcionario_id = int(input("ID do funcionário: "))
+
+print(f"\nID: {id_comanda}")
+print(f"ID Mesa: {mesa_id}")
+print(f"ID Funcionário: {funcionario_id}")
+print(f"Status: aberta")
+print(f"Total: R$ 0.00")
