6ab1d9ea5c8e45239cb4ce307dc8df7d21a31f65
angelo <159036003+angelo5628@users.noreply.github.com>
2025-09-25 23:08:42 -0300

    Create prato_ingredientes.py

---
+++ prato_ingredientes.py
@@ -0,0 +1,11 @@
+ultimo_id = 0
+id_relacao = ultimo_id + 1
+
+prato_id = int(input("ID do prato: "))
+ingrediente_id = int(input("ID do ingrediente: "))
+quantidade = float(input("Quantidade: "))
+
+print(f"\nID: {id_relacao}")
+print(f"ID Prato: {prato_id}")
+print(f"ID Ingrediente: {ingrediente_id}")
+print(f"Quantidade: {quantidade}")
