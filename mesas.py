6d14dce3fbac9aeb8b6b8d23fc2411137a767d76
angelo <159036003+angelo5628@users.noreply.github.com>
2025-09-25 23:09:07 -0300

    Create mesas.py

---
+++ mesas.py
@@ -0,0 +1,10 @@
+ultimo_id = 0
+id_mesa = ultimo_id + 1
+
+numero = int(input("Número da mesa: "))
+capacidade = int(input("Capacidade: "))
+
+print(f"\nID: {id_mesa}")
+print(f"Número: {numero}")
+print(f"Capacidade: {capacidade} pessoas")
+print(f"Status: livre")
