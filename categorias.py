8c4994155e33e24dc2747624f85c14e440b47586
angelo <159036003+angelo5628@users.noreply.github.com>
2025-09-25 23:07:11 -0300

    Create categorias.py

---
+++ categorias.py
@@ -0,0 +1,9 @@
+ultimo_id = 0
+id_categoria = ultimo_id + 1
+
+nome = input("Nome da categoria: ")
+descricao = input("Descrição: ")
+
+print(f"\nID: {id_categoria}")
+print(f"Nome: {nome}")
+print(f"Descrição: {descricao}")
